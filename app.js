2250e630e37bb3dc7634e1c643f739522ebf7e49
RoberGF <rgfrasta@gmail.com>
2025-04-06 18:04:16 +0200

    WIP: login, and format files

--- app.js
+++ app.js
@@ -20,29 +20,29 @@ app.use(logger('dev'));
 // transforms json objects into js objects
 app.use(express.json());
 // transforms data sent by a form to a js object
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 // cookie parser to get cookies from client
 app.use(cookieParser());
 // set the folder where statis resources will be served
 app.use(express.static(join(import.meta.dirname, 'public')));
 
 // Routing
-app.use(routes)
+app.use(routes);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler para APIs (devolvemos JSON en vez de renderizar)
 app.use(function (err, req, res, next) {
-  console.error('❌ Error:', err.message);
+    console.error('❌ Error:', err.message);
 
-  res.status(err.status || 500).json({
-    error: true,
-    message: err.message,
-    stack: process.env.NODE_ENV === 'development' ? err.stack : undefined
-  });
+    res.status(err.status || 500).json({
+        error: true,
+        message: err.message,
+        stack: process.env.NODE_ENV === 'development' ? err.stack : undefined,
+    });
 });
 
 export default app;
--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,2 +1,21 @@
+// import User from '../models/User.js'
+import createError from 'http-errors';
+import bcrypt from 'bcryptjs';
 
-export const login = (req, res) => res.send("login")
+export const login = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+        const userFound = await User.findOne({ email: email.toLowerCase() });
+        const isMatch = await bcrypt.compare(password, userFound.password);
+
+        if (!userFound || !isMatch) {
+            next(createError(401, 'Invalid Credentials 😢'));
+            return;
+        }
+
+        res.json({ message: 'Login success' });
+    } catch (error) {
+        console.log(error);
+        next(createError(500, 'Internal server error'));
+    }
+};
