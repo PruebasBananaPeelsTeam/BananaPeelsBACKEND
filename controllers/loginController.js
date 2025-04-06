8a95c0b8c3e334ecbd953dc97dd0e45d57169966
RoberGF <rgfrasta@gmail.com>
2025-04-06 18:36:44 +0200

    WIP: checking JWT in login

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,10 +1,11 @@
 // import User from '../models/User.js'
 import createError from 'http-errors';
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken'
 
 export const login = async (req, res, next) => {
-    const { email, password } = req.body;
     try {
+        const { email, password } = req.body;
         const userFound = await User.findOne({ email: email.toLowerCase() });
         const isMatch = await bcrypt.compare(password, userFound.password);
 
@@ -13,6 +14,20 @@ export const login = async (req, res, next) => {
             return;
         }
 
+        jwt.sign(
+            { _id: user._id },
+            process.env.JWT_TOKEN,
+            {
+                expiresIn: '2d',
+            },
+            (err, tokenJWT) => {
+                if (err) {
+                    next(err);
+                    return;
+                }
+                res.json({ tokenJWT });
+            },
+        );
         res.json({ message: 'Login success' });
     } catch (error) {
         console.log(error);
