fd4b933c0cb99ea785498e54eff025067ae4f158
9000Noemi <noemi.martinc@gmail.com>
2025-04-07 13:13:46 +0200

    fix registerRoutes

--- routes/registerRoutes.js
+++ routes/registerRoutes.js
@@ -1,8 +1,8 @@
 import express from 'express';
-import { register } from '../controllers/registerController.js';
+import { createUser } from '../controllers/registerController.js';
 
 const router = express.Router();
 
-router.post('/api/register', register);
+router.post('/api/register', createUser);
 
 export default router;
