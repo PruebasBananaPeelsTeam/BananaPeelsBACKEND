3793303098865f9b0e2ebb33a57b80a4b27a3554
Javier Rodriguez <franciscoromera0816@gmail.com>
2025-04-09 20:25:05 +0200

    fix: console.log with snsible info deleted

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,19 +2,21 @@ import jwt from 'jsonwebtoken';
 import User from '../models/User.js';
 
 // Middleware para proteger rutas
+import jwt from 'jsonwebtoken';
+import User from '../models/User.js';
+
+// Middleware para proteger rutas
 export const authMiddleware = async (req, res, next) => {
     try {
         const token = req.headers.authorization?.replace('Bearer ', '');
-        console.log('👉 Token recibido:', token);
 
         if (!token) {
             return res.status(401).json({ error: 'Token required' });
         }
 
         const decoded = jwt.verify(token, process.env.JWT_TOKEN);
-        console.log('🔐 Decodificado:', decoded);
 
-        const user = await User.findById(decoded._id); // <-- asegúrate que usas _id si es lo que pusiste en el payload
+        const user = await User.findById(decoded._id);
 
         if (!user) {
             return res.status(401).json({ error: 'User not found' });
@@ -23,7 +25,7 @@ export const authMiddleware = async (req, res, next) => {
         req.user = user;
         next();
     } catch (error) {
-        console.error('❌ JWT error:', error.message);
+        console.error('❌ JWT error:', error.message); 
         res.status(401).json({ error: 'Invalid Token' });
     }
 };
