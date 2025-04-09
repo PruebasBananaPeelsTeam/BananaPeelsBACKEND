98e1df683d45d302ae112eb91b77afbb54b3a187
RoberGF <rgfrasta@gmail.com>
2025-04-09 13:47:37 +0200

    Update: added login limiter attemps per minute

---
+++ middlewares/loginLimiter.js
@@ -0,0 +1,13 @@
+import rateLimit from 'express-rate-limit';
+
+// limit of 5 login attemps per min 
+export const loginLimiter = rateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 5, 
+  message: {
+    error: true,
+    message: 'Too many login attempts, please try again in 60 seconds.',
+  },
+  standardHeaders: true,
+  legacyHeaders: false,
+});
--- routes/Routes.js
+++ routes/Routes.js
@@ -4,12 +4,13 @@ import { createUser } from '../controllers/registerController.js';
 import { login } from '../controllers/loginController.js';
 import { advertsList } from '../controllers/advertsListController.js'
 import { createAdvert } from '../controllers/create_advertsController.js';
+import { loginLimiter } from '../middlewares/loginLimiter.js';
 
 const router = express.Router();
 
 router.get('/api/adverts', advertsList);
 router.post('/api/adverts', /*authMiddleware,*/ upload.single('image'), createAdvert);
 router.post('/api/register', createUser);
-router.post('/api/login', login);
+router.post('/api/login',loginLimiter, login);
 
 export default router;
