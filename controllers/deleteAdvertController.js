9a30db603eca9c47042d969036b41dcfa10c1d4d
9000Noemi <noemi.martinc@gmail.com>
2025-04-28 10:14:15 +0200

    added deleteAdvertController

---
+++ controllers/deleteAdvertController.js
@@ -0,0 +1,23 @@
+import Advert from '../models/Advert.js';
+
+export async function deleteAdvert (req, res, next) {
+    const advertId = req.params.id;
+    const userId = req.user._id;
+
+    //Buscar el anuncio por id en la base de datos
+    const advert = await Advert.findById(advertId)
+
+    //Si no existe el anuncio devolvemos un error 404 diciendo 'Advert not found', y detenemos la ejecución del servidor para esa petición
+    if (!advert) {
+        return res
+            .status(404)
+            .json({ success: false, message: 'Advert not found' });
+    }
+
+    //Verificar que el usuario es el dueño del anuncio que intenta borrar. Si no lo es, devolvemos un 403 Forbidden
+    if (advert.ownerId.toString() !== userId.toString()) {
+        return res
+        .status(403)
+        .json({ error: 'You are not authorized to delete this advert' });
+    }
+}
